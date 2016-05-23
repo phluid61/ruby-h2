731bcc2028257b23af3b228e9e0cf0fd84954d0d
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-05-23 15:42:44 +1000

    fix some logic in demo library

--- lib/ruby-h2/__01_library.rb
+++ lib/ruby-h2/__01_library.rb
@@ -12,40 +12,55 @@ end
 
 def request_handler r, c
 	q = RUBYH2::HTTPResponse.new r.stream
-	case r.method.upcase
-	when 'GET', 'HEAD'
-		callback = _get[r.path]
-		if callback
-			q.status = 200
-			q['content-type'] = 'text/html'
-			q << callback.call(r, q)
-		else
-			q.status = 404
-			q['content-type'] = 'text/html'
-			q << <<HTML
+	begin
+		case r.method.upcase
+		when 'GET', 'HEAD'
+			callback = _get[r.path]
+			if callback
+				q.status = 200
+				q['content-type'] = 'text/html'
+				q << callback.call(r, q)
+			else
+				q = RUBYH2::HTTPResponse.new r.stream # wipe any changes from the handler
+				q.status = 404
+				q['content-type'] = 'text/html'
+				q << <<HTML
 <!DOCTYPE html>
 <html lang="en"><head><title>Not Found</title></head><body><h1>Not Found</h1><p>Resource <tt>#{r.path}</tt> not found.</p></body></html>
 HTML
-		end
-	else
-			q.status = 405
-			q['content-type'] = 'text/html'
-			q << <<HTML
+			end
+		else
+				q = RUBYH2::HTTPResponse.new r.stream #...
+				q.status = 405
+				q['content-type'] = 'text/html'
+				q << <<HTML
 <!DOCTYPE html>
 <html lang="en"><head><title>Not Allowed</title></head><body><h1>Not Allowed</h1><p>Method <tt>#{r.method}</tt> not allowed.</p></body></html>
 HTML
+		end
+	rescue
+		q = RUBYH2::HTTPResponse.new r.stream #...
+		q.status = 500
+		q['content-type'] = 'text/html'
+				q << <<HTML
+<!DOCTYPE html>
+<html lang="en"><head><title>Internal Server Error</title></head><body><h1>Internal Server Error</h1><p>An error occurred while attempting to handle your request.</p></body></html>
+HTML
 	end
-	c.deliver r
+	c.deliver q
 end
 
 at_exit do
+	require 'threadpuddle'
 	require 'socket'
+	threads = ThreadPuddle.new 100
 	server = TCPServer.new $port
 	loop do
-		# FIXME: GC?
-		hclient = RUBYH2::HTTPClient.new
-		hclient.on_request {|r| request_handler r, hclient }
-		hclient.wrap server.accept
+		threads.spawn do
+			hclient = RUBYH2::HTTPClient.new
+			hclient.on_request {|r| request_handler r, hclient }
+			hclient.wrap server.accept
+		end
 	end
 end
 
