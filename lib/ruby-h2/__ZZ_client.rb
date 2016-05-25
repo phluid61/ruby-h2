6f499451577e75b85dd15ff09a472aa03c42de2d
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-05-25 17:56:59 +1000

    more debuggery in fake client

--- lib/ruby-h2/__ZZ_client.rb
+++ lib/ruby-h2/__ZZ_client.rb
@@ -66,9 +66,39 @@ Thread.pass
 s.xmit PREFACE
 preader.join
 
+#---
+
 require_relative 'frame-types'
 require_relative 'hpack'
 $frame_name = Hash[ RUBYH2::FrameTypes.constants.map{|c| [RUBYH2::FrameTypes.const_get(c), c]} ]
+
+#---
+
+$_open = {}
+$_open_sem = Mutex.new
+def open i
+	$_open_sem.synchronize {
+		$_open[i] = 2
+	}
+end
+def half_close i
+	ded = false
+	$_open_sem.synchronize {
+		$_open[i] -= 1
+		ded = true if $_open[i] == 0
+	}
+	puts "stream #{i} is closed" if ded
+end
+def close i
+	$_open_sem.synchronize {
+		$_open[i] = 0
+	}
+	puts "stream #{i} is closed"
+end
+
+#---
+
+$ignore_EOFError = false
 
 reader = Thread.new do
 	begin
@@ -85,33 +115,39 @@ puts "read #{bytes.bytesize} bytes" rescue nil
 					puts "   flags:  #{frame[:flags].to_s 16}"
 					puts "   stream: #{frame[:sid]}"
 					puts "   payload: [#{frame[:payload].bytesize}]"
-					puts *frame[:payload].each_byte.map{|b|'%02X' % b}.each_slice(8).each_slice(4).map{|q|q.map{|h|h.join ' '}.join '   '}
-					puts ''
-					case frame[:type]
-					when 0
-						puts '---8<---', frame[:payload], '--->8---', ''
-					when 1
-						# parse headers
-						puts '---'
-						hpack.parse_block(frame[:payload]) do |k, v|
-							puts " #{k}: #{v}"
+					if frame[:payload].bytesize > 0
+						puts *frame[:payload].each_byte.map{|b|'%02X' % b}.each_slice(8).each_slice(4).map{|q|q.map{|h|h.join ' '}.join '   '}
+						case frame[:type]
+						when 0
+							puts '---8<---', frame[:payload], '--->8---'
+							half_close frame[:sid] if (frame[:flags] & 0x1) == 0x1
+						when 1
+							# parse headers
+							puts '---'
+							hpack.parse_block(frame[:payload]) do |k, v|
+								puts " #{k}: #{v}"
+							end
+							puts '---'
+							half_close frame[:sid] if (frame[:flags] & 0x1) == 0x1
+						when 3
+							close frame[:sid]
+						when 4
+							bytes = frame[:payload].dup.b
+							puts '---'
+							until bytes.nil? or bytes.empty?
+								k, v, bytes = bytes.unpack 'nNC*'
+								puts " #{'%02X' % k} = #{v}"
+							end
+							puts '---'
 						end
-						puts '---', ''
-					when 3
-						# TODO: RST_STREAM
-					when 4
-						bytes = frame[:payload].dup.b
-						puts '---'
-						until bytes.nil? or bytes.empty?
-							k, v, bytes = bytes.unpack 'nNC*'
-							puts " #{'%02X' % k} = #{v}"
-						end
-						puts '---', ''
 					end
+					puts ''
 				end
 				Thread.pass
 			end#loop
 		end#catch
+	rescue IOError => x
+		STDERR.puts "#{x.class.name}: #{x}", *x.backtrace.map{|bt|"\t#{bt}"} unless $ignore_EOFError
 	rescue Exception => x
 		STDERR.puts "#{x.class.name}: #{x}", *x.backtrace.map{|bt|"\t#{bt}"}
 	end
@@ -127,6 +163,8 @@ s.xmit( [0,8, 0x6, 0x00, 0].pack(HEADER_FORMAT)+'UUUUUUUU' )
 puts "SENT PING"
 
 hpack = RUBYH2::HPack.new
+
+open 1
 # HEADERS (GET /)
 bytes = hpack.create_block({
 	':method' => 'GET',
@@ -136,19 +174,66 @@ bytes = hpack.create_block({
 	'date' => Time.now.utc.strftime('%a, %e %b %Y %H:%M:%S %Z'),
 	'user-agent' => 'TestClient/1.0',
 })
-#p bytes
 len = bytes.bytesize
 len0 = len >> 16
 len1 = len & 0xFFFF
 s.xmit( [len0,len1, 0x1, 0x01|0x04, 1].pack(HEADER_FORMAT)+bytes )
-s.flush
 puts "WROTE HEADERS"
+half_close 1
 
 # test ping2
 s.xmit( [0,8, 0x6, 0x00, 0].pack(HEADER_FORMAT)+'33333333' )
 puts "SENT PING"
 
+sleep 1
+
+open 3
+# HEADERS (GET /nonesuch)
+bytes = hpack.create_block({
+	':method' => 'GET',
+	':scheme' => 'http',
+	':path' => '/nonesuch',
+	'host' => 'localhost',
+	'date' => Time.now.utc.strftime('%a, %e %b %Y %H:%M:%S %Z'),
+	'user-agent' => 'TestClient/1.0',
+})
+len = bytes.bytesize
+len0 = len >> 16
+len1 = len & 0xFFFF
+s.xmit( [len0,len1, 0x1, 0x01|0x04, 3].pack(HEADER_FORMAT)+bytes )
+puts "WROTE HEADERS"
+half_close 3
+
+sleep 0.5
+
+open 5
+# HEADERS (POST /)
+payload = 'foobar'
+bytes = hpack.create_block({
+	':method' => 'POST',
+	':scheme' => 'http',
+	':path' => '/',
+	'host' => 'localhost',
+	'date' => Time.now.utc.strftime('%a, %e %b %Y %H:%M:%S %Z'),
+	'user-agent' => 'TestClient/1.0',
+	'content-type' => 'text/plain',
+	'content-length' => payload.bytesize.to_s,
+})
+len = bytes.bytesize
+len0 = len >> 16
+len1 = len & 0xFFFF
+s.xmit( [len0,len1, 0x1, 0x04, 5].pack(HEADER_FORMAT)+bytes )
+puts "WROTE HEADERS"
+# BODY (POST /)
+len = payload.bytesize
+len0 = len >> 16
+len1 = len & 0xFFFF
+s.xmit( [len0,len1, 0x0, 0x01, 5].pack(HEADER_FORMAT)+payload )
+puts "WROTE DATA"
+half_close 3
+
 sleep 5
+$ignore_EOFError = true
 s.close
 
 reader.join
