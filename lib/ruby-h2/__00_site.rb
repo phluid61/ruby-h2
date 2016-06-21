41f16be2124132a663ee870dea0696d729d5b468
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-06-21 13:04:26 +1000

    add some HTML to the test Site

--- lib/ruby-h2/__00_site.rb
+++ lib/ruby-h2/__00_site.rb
@@ -9,7 +9,7 @@ Application.https = true
 get '/' do
 	<<HTML
 <!DOCTYPE html>
-<html lang="en"><head><title>Index</title></head><body><h1>Index</h1><p>This is the index.</p></body></html>
+<html lang="en"><head><title>Index</title></head><body><h1>Index</h1><p>This is the index.</p><ul><li><a href="/">simple test</a></li><li><a href="/padded">padding test</a></li></ul></body></html>
 HTML
 end
 
@@ -17,7 +17,7 @@ get '/padded' do |request, response|
 	response.pad!
 	<<HTML
 <!DOCTYPE html>
-<html lang="en"><head><title>Padded</title></head><body><h1>Padded</h1><p>This response should be padded.</p></body></html>
+<html lang="en"><head><title>Padded</title></head><body><h1>Padded</h1><p>This response should be padded.</p><ul><li><a href="/">simple test</a></li><li><a href="/padded">padding test</a></li></ul></body></html>
 HTML
 end
 
