594401e625c1dc11f15c18d9b3e3aa4fc1d4e5aa
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-06-22 17:13:55 +1000

    make a real web client

---
+++ client.rb
@@ -0,0 +1,101 @@
+# encoding: BINARY
+# vim: ts=2 sts=2 sw=2 expandtab
+
+require 'optparse'
+opts = {
+  :host => 'google.com',
+  :port => 443,
+  :https => true,
+}
+OptionParser.new do |o|
+  o.banner = "Usage: ruby #{$0} [options]"
+  o.on('-h', '--host HOST', 'The server name to connect to') {|h| opts[:host] = h }
+  o.on('-p', '--port PORT', OptionParser::DecimalInteger, 'The TCP port to connect to') {|p| opts[:port] = Integer(p) }
+  o.on('-s', '--[no-]https', 'Whether or not to use HTTPS') {|s| opts[:https] = s }
+  o.on('-?', '--help', 'Show this help message, and quit') { puts o; exit }
+end.parse!
+
+
+require 'socket'
+s = TCPSocket.new opts[:host], opts[:port]
+s.setsockopt(Socket::IPPROTO_TCP, Socket::TCP_NODELAY, 1)
+
+$authority = opts[:host]
+if opts[:https]
+  $scheme = 'https'
+  $authority += ":#{opts[:port]}" if opts[:port] != 443
+  require 'openssl'
+  ctx = OpenSSL::SSL::SSLContext.new :TLSv1_2_client
+  ctx.verify_callback = lambda {|pverify_ok, store_context| true } # security!
+  if ctx.respond_to? :alpn_protocols=
+    ctx.alpn_protocols = %w[h2]
+  else
+    STDERR.puts "OpenSSL version doesn't support ALPN"
+  end
+  s = OpenSSL::SSL::SSLSocket.new s, ctx
+  if s.respond_to? :hostname=
+    s.hostname = opts[:host]
+  else
+    STDERR.puts "OpenSSL version doesn't support SNI"
+  end
+  s.sync_close = true
+  s.connect
+else
+  $scheme = 'http'
+  $authority += ":#{opts[:port]}" if opts[:port] != 80
+end
+
+require 'logger'
+logger = Logger.new STDERR
+logger.progname = 'demo-client'
+logger.datetime_format = '%Y-%m-%d %H:%M:%S'
+logger.sev_threshold = Logger::DEBUG
+
+require_relative 'lib/ruby-h2/http-agent'
+require_relative 'lib/ruby-h2/http-request'
+
+$shutdown = false
+agent = RUBYH2::HTTPAgent.new(false, logger)
+agent.on_response do |r|
+  #puts "RECEIVED RESPONSE: #{r.inspect}"
+  puts '--'
+  r.headers.each_pair do |k,v|
+    puts "#{k}: [#{v}]"
+  end
+  puts ''
+  #puts r.body
+  #puts '--'
+end
+wrapper = Thread.new do
+  begin
+    agent.wrap s
+  rescue Exception => e
+    if !$shutdown
+      logger.error "#{e.class.name}: #{e}"
+      STDERR.puts "#{e.class.name}: #{e}", *e.backtrace.map{|bt|"\t#{bt}"}
+    end
+  end
+end
+
+agent.ping 'UUUUUUUU'
+
+headers = {
+  ':scheme' => $scheme,
+  ':authority' => $authority,
+  'host' => opts[:host],
+  'user-agent' => 'TestClient/1.0',
+}
+agent.deliver RUBYH2::HTTPRequest.new(1, 'GET', '/', headers)
+
+agent.ping '33333333'
+
+sleep 5
+
+$shutdown = true
+agent.shut_down
+
+sleep 2
+s.close
+
+wrapper.join
+
