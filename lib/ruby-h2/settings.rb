1497f6ac931583bbd17d761e9a796d61e9846a68
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2016-07-05 16:56:24 +1000

    proper Connection Error on invalid SETTINGS size

--- lib/ruby-h2/settings.rb
+++ lib/ruby-h2/settings.rb
@@ -3,6 +3,7 @@
 
 require_relative 'frame'
 require_relative 'frame-types'
+require_relative 'errors'
 
 module RUBYH2
 	module Settings
@@ -28,10 +29,14 @@ module RUBYH2
 
 			def pairs_from frame
 				raise if frame.type != FrameTypes::SETTINGS #FIXME
+        # RFC 7540, Section 6.5
+        # "A SETTINGS frame with a length other than a multiple of 6
+        #  octets MUST be treated as a connection error (Section 5.4.1)
+        #  of type FRAME_SIZE_ERROR."
+        raise ConnectionError.new(Error::PROTOCOL_ERROR, "SETTINGS payload should be multiple of 6 bytes, got #{bytes.bytesize} (#{bytes.bytesize % 6})") if bytes.bytesize % 6 != 0
 				hash = {}
 				bytes = frame.payload
 				while !bytes.empty?
-					raise if bytes.bytesize < 6
 					k, v, bytes = bytes.unpack('nNa*')
 					hash[k] = v
 				end
